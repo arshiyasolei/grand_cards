f52b7d9445600e2ef533dd63f0d7d43ea291a62f
arshia.solei <dogbles@dogbless-iMac-Pro.local>
2020-06-29 20:47:25 -0700

    Swtiched to html5 canvas

--- main.js
+++ main.js
@@ -1,3 +1,19 @@
-function change_section_text(something) {
-    document.getElementById("change").innerText = "I got changed!" + something.id
+var c;
+var ctx
+window.onload = function() {
+
+    c = document.getElementById("main_canvas_view");
+
+    ctx = c.getContext("2d");
+    ctx.canvas.width = window.innerWidth;
+    ctx.canvas.height = window.innerHeight;
+    for (i = 1; i < 6; i++) {
+        card(10 * (i * 14), 10, 100, 300);
+    }
+}
+
+function card(x, y, w, h) {
+    ctx.beginPath();
+    ctx.rect(x, y, w, h);
+    ctx.stroke();
 }
