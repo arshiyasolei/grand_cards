d67acd6897767da1a609682f33f07bd3bb50d19b
arshiyasolei <arshiyasoleimani@gmail.com>
2020-07-07 16:28:06 -0700

    refactoring

--- game.js
+++ game.js
@@ -1,4 +1,27 @@
 window.onload = function () {
+  let socket = io.connect('http://localhost:8080');
+
+  // submit text message without reload/refresh the page
+  $('form').submit(function (e) {
+    e.preventDefault(); // prevents page reloading
+    socket.emit('chat_message', $('#txt').val());
+    $('#txt').val('');
+    return false;
+  });
+
+  // append the chat text message
+  socket.on('chat_message', function (msg) {
+    $('#messages').append($('<li>').html(msg));
+  });
+
+  // append text if someone is online
+  socket.on('is_online', function (username) {
+    $('#messages').append($('<li>').html(username));
+  });
+
+  // ask username
+  var username = prompt('Please tell me your name');
+  socket.emit('username', username);
   // get canvas related references
   let canvas = document.getElementById("main_canvas_view");
   let ctx = canvas.getContext("2d");
